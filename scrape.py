b392cbc8839deaeb6726b4603a393fd6fd404496
Kenneth Lee <leeoku@gmail.com>
2019-09-11 23:18:30 -0400

    Add messaging

--- scrape.py
+++ scrape.py
@@ -1,7 +1,8 @@
 from urllib.request import urlopen,Request
 from bs4 import BeautifulSoup
+import os
 
-url = 'https://www.carsontoyota.com/auto/used-2010-volkswagen-gti-4dr-hb-pzev-at-carson-ca/35331342/'
+url = input('Enter car link: ')
 req = Request(url, headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11', 
 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3', 'Accept-Encoding': 'none',
@@ -9,9 +10,36 @@ req = Request(url, headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) Appl
 'Connection': 'keep-alive'})
 page = urlopen(req).read()
 soup = BeautifulSoup(page,features = "lxml")
+
+aPrice = soup.findAll("",{"class":"vehicle_price_result"})
+if aPrice[0]:
+    print (aPrice[0].text)
+
+divs = soup.findAll('div', class_="pre_owned_left_div")
+for div in divs:
+    div.find()
+print(divs)
+
+
+
+'''
+table = soup.find('div',{'class':"PreOwnedCarCategoryDetail"})
+table_body = table.find('tr')
+
+
+print(table_body)
+rows = table_body.findAll('details-overview_data')
+
+pricelist = []
+for row in rows:
+    pricelist.append(row)
+print(pricelist)
+'''
+'''
 for price in soup.findAll("",{"class":"vehicle_price"}):
     try:
         print (price.text)
         break
     except KeyError:
         pass
+'''
